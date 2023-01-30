5d88b68a139102bbca230e5abd1cb36359b67c2d
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2023-01-30 14:15:06 +0600

    updated

--- index.js
+++ index.js
@@ -4,20 +4,10 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const app = express();
-// const checkScopes = requiredScopes("read:messages");
 const port = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
-// const { auth } = require("express-oauth2-jwt-bearer");
-// app.use(
-//   auth({
-//     issuer: "https://YOUR_ISSUER_DOMAIN",
-//     audience: "https://my-api.com",
-//     secret: "YOUR SECRET",
-//     tokenSigningAlg: "HS256",
-//   })
-// );
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njxwr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -25,13 +15,6 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
-// Authorization middleware. When used, the Access Token must
-// exist and be verified against the Auth0 JSON Web Key Set.
-// const checkJwt = auth({
-//   audience: "YOUR_API_IDENTIFIER",
-//   issuerBaseURL: `https://YOUR_DOMAIN/`,
-// });
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -53,43 +36,21 @@ async function run() {
     await client.connect();
     const powerCollection = client.db("powerHack").collection("billing-list");
 
-    app.post("/api/login", async (req, res) => {
+    app.post("/api/login", verifyJWT, async (req, res) => {
       const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
-      });
-      res.send({ accessToken });
-    });
-    app.post("/api/registration", async (req, res) => {
-      const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
+        expiresIn: "2h",
       });
       res.send({ accessToken });
     });
 
-    // This route doesn't need authentication
-    app.get("/api/public", function (req, res) {
-      res.json({
-        message:
-          "Hello from a public endpoint! You don't need to be authenticated to see this.",
+    app.post("/api/registration", verifyJWT, async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
       });
+      res.send({ accessToken });
     });
-
-    // This route needs authentication
-    // app.get("/api/private", checkJwt, function (req, res) {
-    //   res.json({
-    //     message:
-    //       "Hello from a private endpoint! You need to be authenticated to see this.",
-    //   });
-    // });
-
-    // app.get("/api/private-scoped", checkJwt, checkScopes, function (req, res) {
-    //   res.json({
-    //     message:
-    //       "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
-    //   });
-    // });
 
     app.get("/api/billing-list", async (req, res) => {
       const query = {};
@@ -119,31 +80,23 @@ async function run() {
       const count = await powerCollection.estimatedDocumentCount();
       res.send({ count });
     });
+
     app.post("/api/billing-list", async (req, res) => {
       const newBill = req.body;
       const result = await powerCollection.insertOne(newBill);
       res.send(result);
     });
-    app.put("/api/update-billing/:id", async (req, res) => {
+
+    // update billing
+    app.patch("/api/update-billing/:id", async (req, res) => {
       const id = req.params.id;
-      const billingData = req.body;
+      const body = req.body;
       const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          name: billingData.name,
-          email: billingData.email,
-          amount: billingData.amount,
-          phone: billingData.phone,
-        },
-      };
-      const result = await profileCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
+      const updateDoc = { $set: body };
+      const result = await powerCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
     app.delete("/api/delete-billing/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
